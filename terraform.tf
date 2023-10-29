d1bd7bf4e890baac74ad0c44121150c5e5c3b4be
unknown <dev67574@gmail.com>
2023-10-28 23:28:31 -0600

    Remove cloud block2

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,14 @@
 terraform {
 
+/*
   cloud {
-    organization = "organization-name"
+    organization = "terraformcloudliesofp"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
